60b1c735726415bb77af8e8e055f4ebc7bc3ce08
Bentzy <ben.ro@gloat.com>
2022-06-04 09:45:13 +0300

    tabs and design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,28 +5,31 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center text-lg-h3">
+      <div class="d-flex align-center text-lg-h3 mr-6">
          <v-icon class="text-lg-h3 mr-3">mdi-robot-happy</v-icon> MafiaBot
       </div>
+      <v-select class="mt-6 mr-4" placeholder="Select a Player" v-model="selected_player_id" :items="players"></v-select>
 
-      <v-spacer></v-spacer>
-
-      <v-select class="mt-6" placeholder="Select a Player" v-model="selected_player_id" :items="players"></v-select>
     </v-app-bar>
 
-    <v-main class="ma-5">
+    <v-main class="ma-5 mb-0">
       <div v-if="selected_player_id">
-        <h2>Single Month Data</h2>
-        <LineChart class="chart" :chart-data="get_player_data('single_month_data')" :chart-options="chartOptions" />
-
-        <h2 class="mt-16">All Data</h2>
-        <LineChart class="chart" :chart-data="get_player_data('upto_month_data')" :chart-options="chartOptions" />
+          <v-tabs class="mb-3" v-model="cpage">
+            <v-tab>Data Over Time</v-tab>
+            <v-tab>Single Night Data</v-tab>
+          </v-tabs>
+        <v-carousel height="auto" hide-delimiters :show-arrows="false" v-model="cpage">
+          <v-carousel-item>
+            <LineChart class="chart" :chart-data="get_player_data('upto_month_data')" :chart-options="chartOptions" />
+          </v-carousel-item>
+          <v-carousel-item>
+            <LineChart class="chart" :chart-data="get_player_data('single_month_data')" :chart-options="chartOptions" />
+          </v-carousel-item>
+        </v-carousel>
       </div>
-      <div v-else>
-        <h4>Hey its me <strong>Martinez</strong> the Mafia-Bot
-        <br>
-          Please choose a player from the list on the top right to proceed
-        </h4>
+      <div v-else class="align-content-center align-center text-center">
+        <h2 STYLE="font-weight: 300">Hi, I am <strong>Martinez</strong> the <strong>MafiaBot</strong></h2>
+        <div style="font-weight: 300;" class="mt-2">Please choose a player from the list on the top navbar</div>
         <img class="martinez" src="./assets/martinez.png">
       </div>
     </v-main>
@@ -46,14 +49,20 @@ export default {
   components: {
     LineChart
   },
-
+  mounted(){
+    // this.$vuetify.theme.dark = true
+    // this.$vuetify.theme.themes.dark.primary = '#b45294'
+  },
   data: () => ({
     mafia_data: MafiaData,
     selected_player_id: null,
     chartOptions: {
       type: Object,
-      default: () => {}
-    }
+      default: () => {
+      },
+      scales: {y: {beginAtZero: true}},
+    },
+    cpage: 0,
   }),
   methods: {
     get_player_data(data_type){
@@ -64,7 +73,10 @@ export default {
       }
       let total_points_dataset = {
         label: 'Total Points',
-        data: []
+        data: [],
+        backgroundColor: "transparent",
+        pointBackgroundColor: "rgb(255,255,255)",
+        borderColor: this.$vuetify.theme.themes.dark.primary
       }
       for (let [month_number, players_data] of Object.entries(this.mafia_data[data_type])) {
         chart_data.labels.push(month_number)
@@ -97,12 +109,15 @@ export default {
 
 <style>
   .chart canvas{
-    max-height: 80vh;
+    max-height: 65vh;
   }
   .martinez{
-    max-height: 90vh;
+    max-height: 100vh;
     max-width: 100vw;
     position: fixed;
     bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
   }
+
 </style>
